b94d70f1766e870848cae84aa61f17bd9fc3cc8b
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-09 12:34:08 +0300

    was done ex4

--- ConsoleApp_09_06/Program.cs
+++ ConsoleApp_09_06/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    // Унікальне ім'я м'ютекса для ідентифікації додатка
+    private const string MutexName = "Global\\MySingleInstanceApp";
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        using (Mutex mutex = new Mutex(false, MutexName, out bool createdNew))
+        {
+            if (!createdNew)
+            {
+                Console.WriteLine("Програма вже запущена. Друга копія не може бути запущена.");
+                return;
+            }
+
+            // Основний код додатка
+            Console.WriteLine("Програма запущена. Натисніть Enter для завершення роботи.");
+            Console.ReadLine();
+        }
+    }
+}
